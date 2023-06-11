bdec1aebe891162ab1aa7f1a2dc21847fc0965c2
Vinicius Oliveira <vinicius.developer@outlook.com>
2023-06-11 19:45:27 -0300

    Rotas

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@ import express from 'express'
 
 const app = express()
 
-app.listen(6000, () => console.log("Servidor rodando na porta 6000"))
+app.get("/livros", (req, res) => {
+  //devolver uma lista de livros
+
+  console.log(req.query)
+
+  if (req.query.categoria === 'acao') {
+    return res.json(["livro de acao"])
+  }
+
+  const livros = ["livro 1", "livro 2"]
+
+  res.json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+
+  res.json({ id: req.params.id, nome: "livro tal", categoria: "acao" })
+})
+
+
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
+
+export default app
